d42cb16c2b45572eb6552a9dd8351c6652d9038e
Mihai Nechifor <Mihai.Nechifor@endava.com>
2019-01-25 20:37:13 +0200

    updated code

--- AdoNetExamples/AdoNetExamples/Program.cs
+++ AdoNetExamples/AdoNetExamples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace AdoNetExamples
@@ -17,30 +18,63 @@ namespace AdoNetExamples
 
             //Console.WriteLine(connection.ServerVersion);
 
-            Console.WriteLine("Please enter book name:");
-            string bookName = Console.ReadLine();
+           // Console.WriteLine("Please enter book name:");
+           // string bookName = Console.ReadLine();
 
-            string selectStatement = "select count(*) from book";
-            SqlCommand command = new SqlCommand(selectStatement)
+            try
             {
-                Connection = connection
-            };
+                string selectStatement = "select count(*) from book11";
+                SqlCommand command = new SqlCommand(selectStatement)
+                {
+                    Connection = connection
+                };
 
-            int o = (int)command.ExecuteScalar();
+                int o = (int) command.ExecuteScalar();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+               // connection.Close();
+            }
 
-            InsertNewBook(connection, bookName);
+            //InsertNewBook(connection, bookName);
+            
+            string selectAllBooks = "GetAllBooks";
+            SqlCommand selectCommand = new SqlCommand(selectAllBooks);
+            selectCommand.Connection = connection;
+
+            selectCommand.CommandType = CommandType.StoredProcedure;
+
+            SqlDataReader reader = selectCommand.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                string book = $"{reader["Id"]}, {reader[1]}";
+                Console.WriteLine(book);
+            }
+
+            reader.Close();
+            
+            connection.Close();
 
             Console.ReadLine();
         }
 
         private static void InsertNewBook(SqlConnection connection, string bookName)
         {
-            SqlCommand insertCommand = new SqlCommand();
-            insertCommand.Connection = connection;
+            SqlCommand insertCommand = new SqlCommand
+            {
+                Connection = connection
+            };
 
-            SqlParameter bookParameter = new SqlParameter();
-            bookParameter.Value = bookName;
-            bookParameter.ParameterName = "bookName";
+            SqlParameter bookParameter = new SqlParameter
+            {
+                Value = bookName,
+                ParameterName = "bookName"
+            };
 
             string insertText = @"INSERT INTO [dbo].[Book]
                                 ([Title]
@@ -49,17 +83,15 @@ namespace AdoNetExamples
                                     ,[HardCover]
                                     ,[AuthorID])
                                   VALUES
-                                    (@bookName
-                                        , 2018
-                                        , 300
-                                        , 1
-                                        , 1);
+                                    (@bookName, 2018, 300, 1, 1);
             SELECT CAST(scope_identity() AS int)";
 
             insertCommand.CommandText = insertText;
             insertCommand.Parameters.Add(bookParameter);
 
             int Id = (int) insertCommand.ExecuteScalar();
+
+            Console.WriteLine($"Inserted BookID {Id}");
         }
     }
 }
