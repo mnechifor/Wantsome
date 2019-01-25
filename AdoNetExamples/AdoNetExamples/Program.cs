41a4139a6a382dfb8bb283ce1822b1e3a536a5e9
Mihai Nechifor <Mihai.Nechifor@endava.com>
2019-01-25 18:59:05 +0200

    ADO net examples

---
+++ AdoNetExamples/AdoNetExamples/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AdoNetExamples
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string connectionString = "Data Source=.;Initial Catalog=Library;Integrated Security=True";
+            SqlConnection connection = new SqlConnection
+            {
+                ConnectionString = connectionString
+            };
+
+            connection.Open();
+
+            //Console.WriteLine(connection.ServerVersion);
+
+            string selectStatement = "select count(*) from book";
+            SqlCommand command = new SqlCommand(selectStatement)
+            {
+                Connection = connection
+            };
+
+            int o = (int)command.ExecuteScalar();
+
+            SqlCommand insertCommand = new SqlCommand();
+            insertCommand.Connection = connection;
+
+            string insertText = @"INSERT INTO [dbo].[Book]
+                                ([Title]
+                                    ,[YearOfPublishing]
+                                    ,[NumberOfPages]
+                                    ,[HardCover]
+                                    ,[AuthorID])
+                                  VALUES
+                                    ('C# Programming'
+                                        , 2018
+                                        , 300
+                                        , 1
+                                        , 1)";
+
+            insertCommand.CommandText = insertText;
+
+            insertCommand.ExecuteNonQuery();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExceptionExample/ExceptionExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Method2(i);
+
+            
+        }
+
+        static void Method2(int i)
+        {
+            i = 8;
+        }
+
+        static void Method(out int i)
+        {
+            i = 8;
+        }
+    }
+
+    class Author
+    {
+        
+    }
+}
